a5648ec7048e1a4f9d7f9f12781a6d0f719a9ae1
grant baird <grantbaird_93@hotmail.co.uk>
2021-10-13 18:07:57 +0100

    Add project files.

---
+++ ComputerShareCTCore/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ComputerShareCTCore.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
